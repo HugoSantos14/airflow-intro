30a5903f225e5f564f48291a2caedabfae02ee82
HugoSantos14 <hugosantos14000@gmail.com>
2025-10-15 23:01:28 -0300

    feat(dags): create sensor to verify if the fake user api is available

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,5 +1,6 @@
-from airflow.sdk import dag
+from airflow.sdk import dag, task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.sdk.bases.sensor import PokeReturnValue
 
 @dag
 def user_processing():
@@ -17,4 +18,20 @@ def user_processing():
             )
         """
     )
+
+    @task.sensor(
+        poke_interval=30,   # O sensor vai verificar a condição a cada 30 segundos.
+        timeout=300         # Se todas as tentativas dentro de 5 minutos retornarem falso, o sensor falha.
+    )
+    def is_api_available() -> PokeReturnValue:
+        import requests
+        response = requests.get("https://raw.githubusercontent.com/marclamberti/datasets/refs/heads/main/fakeuser.json")
+
+        is_available = response.status_code == 200
+        fake_user = response.json() if is_available else None
+
+        return PokeReturnValue(is_done=is_available, xcom_value=fake_user)
+
+    is_api_available()
+
 user_processing()
