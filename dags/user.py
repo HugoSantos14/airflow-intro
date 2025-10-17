07598eb6e93bde7009ea065ffce4db8d8dfda5ee
HugoSantos14 <hugosantos14000@gmail.com>
2025-10-17 17:39:45 -0300

    refactor(assets): define user info function for avoid duplicate code

--- dags/user.py
+++ dags/user.py
@@ -1,5 +1,13 @@
 from airflow.sdk import asset, Asset, Context
 
+def user_info(user: Asset, context: Context, info: str) -> dict[str]:
+    user_data = context["ti"].xcom_pull(
+        dag_id=user.name,
+        task_ids=user.name,
+        include_prior_dates=True,
+    )
+    return user_data["results"][0][info]
+
 @asset(
     schedule="@daily",
     uri="https://randomuser.me/api/"
@@ -12,20 +20,10 @@ def user(self) -> dict[str]:
     schedule=user   # Assim que o user for materializado, este asset também será
 )
 def user_location(user: Asset, context: Context) -> dict[str]:
-    user_data = context["ti"].xcom_pull(
-        dag_id=user.name,
-        task_ids=user.name,
-        include_prior_dates=True,
-    )
-    return user_data["results"][0]["location"]
+    return user_info(user, context, "location")
 
 @asset(
     schedule=user
 )
 def user_login(user: Asset, context: Context) -> dict[str]:
-    user_data = context["ti"].xcom_pull(
-        dag_id=user.name,
-        task_ids=user.name,
-        include_prior_dates=True,
-    )
-    return user_data["results"][0]["login"]
+    return user_info(user, context, "login")
