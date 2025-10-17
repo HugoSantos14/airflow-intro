7eb8daabe782f9ab810bb66d411d26cd9cc900b8
HugoSantos14 <hugosantos14000@gmail.com>
2025-10-17 17:48:38 -0300

    refactor(assets): create multiple assets with user_info function

--- dags/user.py
+++ dags/user.py
@@ -1,13 +1,5 @@
 from airflow.sdk import asset, Asset, Context
 
-def user_info(user: Asset, context: Context, info: str) -> dict[str]:
-    user_data = context["ti"].xcom_pull(
-        dag_id=user.name,
-        task_ids=user.name,
-        include_prior_dates=True,
-    )
-    return user_data["results"][0][info]
-
 @asset(
     schedule="@daily",
     uri="https://randomuser.me/api/"
@@ -16,14 +8,20 @@ def user(self) -> dict[str]:
     import requests
     return requests.get(self.uri).json()
 
-@asset(
-    schedule=user   # Assim que o user for materializado, este asset também será
+@asset.multi(
+    schedule=user,
+    outlets=[
+        Asset(name="user_location"),
+        Asset(name="user_login"),
+    ]
 )
-def user_location(user: Asset, context: Context) -> dict[str]:
-    return user_info(user, context, "location")
-
-@asset(
-    schedule=user
-)
-def user_login(user: Asset, context: Context) -> dict[str]:
-    return user_info(user, context, "login")
+def user_info(user: Asset, context: Context) -> list[dict[str]]:
+    user_data = context["ti"].xcom_pull(
+        dag_id=user.name,
+        task_ids=user.name,
+        include_prior_dates=True,
+    )
+    return [
+        user_data["results"][0]["location"],
+        user_data["results"][0]["login"]
+    ]
